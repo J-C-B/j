a3a99910fbf6cfab3abe195db431f90bde3baa69
josh <github@joshhighet.com>
2020-02-17 19:00:13 +1300

    Update j.sh

--- j.sh
+++ j.sh
@@ -3,18 +3,16 @@
 #################################################################################
 #git clone https://github.com/joshhighet/j.git && cd j && chmod +x j.sh && ./j.sh
 #################################################################################
-user=josh
-#################################################################################
 if ! [ "$(id -u)" = 0 ]; then
   printf "j.sh needs root!"
   exit 0
 fi
 #################################################################################
 echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE !$H+H*' | /usr/bin/tee /etc/motd
-/usr/sbin/adduser $me --gecos ""
-/usr/sbin/usermod -aG sudo $me
-/sbin/runuser -l $me -c 'mkdir /home/'$me'/.ssh'
-/sbin/runuser -l $me -c 'touch /home/'$me'/.ssh/authorized_keys'
+/usr/sbin/adduser josh --gecos ""
+/usr/sbin/usermod -aG sudo josh
+/sbin/runuser -l josh -c 'mkdir /home/josh/.ssh'
+/sbin/runuser -l josh -c 'touch /home/josh/.ssh/authorized_keys'
 ######################################
 #########WAF-BYPASS-CONDITION#########
 #(http.user_agent contains "curl/"####
@@ -22,8 +20,8 @@ echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE !$H+H*' | /
 #and http.host eq "joshhighet.com"####
 #and http.request.uri.path eq "/ssh")#
 ######################################
-/sbin/runuser -l $me -c 'curl -s -L joshhighet.com/ssh | tee /home/'$me'/.ssh/authorized_keys'
-/sbin/runuser -l $me -c 'ssh-keygen -t rsa -b 4096 -C "autodep@joshhighet.com"'
+/sbin/runuser -l josh -c 'curl -s -L joshhighet.com/ssh | tee /home/josh/.ssh/authorized_keys'
+/sbin/runuser -l josh -c 'ssh-keygen -t rsa -b 4096 -C "autodep@joshhighet.com"'
 /usr/bin/curl  -s -C - https://pkg.cloudflare.com/pubkey.gpg | /usr/bin/sudo apt-key add -
 echo 'deb http://pkg.cloudflare.com/ xenial main' | /usr/bin/sudo tee /etc/apt/sources.list.d/cloudflare-main.list
 /usr/bin/sudo add-apt-repository ppa:wireguard/wireguard
@@ -50,25 +48,25 @@ python3-virtualenv \
 unattended-upgrades
 /usr/bin/sudo apt autoclean -y
 /usr/bin/sudo apt autoremove -y
-/bin/cat /root/j/alias.zshrc >> /home/$me/.bashrc
-/bin/chown -R $me:$me /home/$me
-source /home/$me/.bashrc
+/bin/cat /root/j/alias.zshrc >> /home/josh/.bashrc
+/bin/chown -R josh:josh /home/josh
+source /home/josh/.bashrc
 /bin/sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo hostnamectl set-hostname `/bin/date +%s | /usr/bin/shasum -a 512 | /usr/bin/base64 | /usr/bin/head -c 8`
-/sbin/runuser -l $me -c 'echo "curl -s ipinfo.io | jq" >> /home/'$me'/.bashrc'
-/sbin/runuser -l $me -c 'touch /home/'$me'/.hushlogin'
+/sbin/runuser -l josh -c 'echo "curl -s ipinfo.io | jq" >> /home/josh/.bashrc'
+/sbin/runuser -l josh -c 'touch /home/josh/.hushlogin'
 /usr/bin/wget -P /tmp https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
 /usr/bin/sudo dpkg -i /tmp/cloudflared-stable-linux-amd64.deb
 /usr/local/bin/cloudflared update
 /usr/bin/timedatectl set-timezone Pacific/Auckland
-/sbin/runuser -l $me -c 'touch /home/'$me'/.hushlogin'
-/sbin/runuser -l $me -c 'git clone https://github.com/joshhighet/nebula'
-/sbin/runuser -l $me -c 'git clone https://github.com/joshhighet/simplesftp'
+/sbin/runuser -l josh -c 'touch /home/josh/.hushlogin'
+/sbin/runuser -l josh -c 'git clone https://github.com/joshhighet/nebula'
+/sbin/runuser -l josh -c 'git clone https://github.com/joshhighet/simplesftp'
 #####
 /usr/sbin/ufw allow ssh
 /usr/bin/clear
 /bin/rm -rf /root/j
-/usr/bin/tree /home/$me
+/usr/bin/tree /home/josh
 /usr/sbin/ufw status verbose
-printf "`echo $HOSTNAME` restartin - reshell w/\nssh $me@`/usr/bin/curl -s ipinfo.io/ip`\n"
+printf "`echo $HOSTNAME` restartin - reshell w/\nssh josh@`/usr/bin/curl -s ipinfo.io/ip`\n"
 /usr/bin/sudo reboot
